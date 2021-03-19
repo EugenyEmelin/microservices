c39db1d1ea1e2a864539f026e778a2a5eae5a652
EugenyEmelin <emelin.eug@ya.ru>
2021-03-19 13:18:27 +0300

    monitoring-1

--- bash-scripts/init-docker-machine.sh
+++ bash-scripts/init-docker-machine.sh
@@ -1,6 +1,6 @@
 docker-machine create --driver google \
 --google-project docker-307118 \
 --google-machine-image https://www.googleapis.com/compute/v1/projects/ubuntu-os-cloud/global/images/family/ubuntu-2004-lts \
---google-machine-type e2-micro \
+--google-machine-type n1-standart-1 \
 --google-zone europe-west1-b \
 docker-host
--- bash-scripts/install-docker-machine.sh
+++ bash-scripts/install-docker-machine.sh
@@ -1,4 +1,4 @@
 base=https://github.com/docker/machine/releases/download/v0.16.0 &&
 mkdir -p "$HOME/bin" &&
 curl -L $base/docker-machine-Windows-x86_64.exe > "$HOME/bin/docker-machine.exe" &&
-chmod +x "$HOME/bin/docker-machine.exe"
+chmod +x "$HOME/bin/docker-machinwe.exe"
