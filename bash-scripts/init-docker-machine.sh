a71fa79aaa25a1f23b4095f2bd78e1122976f593
EugenyEmelin <emelin.eug@ya.ru>
2021-03-22 22:10:50 +0300

    -

--- bash-scripts/init-docker-machine.sh
+++ bash-scripts/init-docker-machine.sh
@@ -1,6 +1,5 @@
 docker-machine create --driver google \
 --google-project docker-307118 \
---google-machine-image https://www.googleapis.com/compute/v1/projects/ubuntu-os-cloud/global/images/family/ubuntu-2004-lts \
---google-machine-type n1-standart-1 \
---google-zone europe-west1-b \
+--google-machine-type e2-micro \
+--google-zone europe-west3-c \
 docker-host
